2c5877396ed99c902a5c177db73b4082fda5c947
Ifus0210 <ifus0210@gmail.com>
2025-06-26 18:11:48 -0700

    enable wasd movement

--- b.py
+++ b.py
@@ -3,7 +3,7 @@ from ursina import *
 app = Ursina()
 
 # 큐브 하나 만들기
-cube = Entity(model='cube', color=color.azure, scale=(2,2,2))
+cube = Entity(model='cube', color=color.azure, scale=(5,5,5))
 
 # 키 입력 처리 함수
 def update():
@@ -11,5 +11,11 @@ def update():
         cube.x -= 2 * time.dt
     if held_keys['d']:
         cube.x += 2 * time.dt
+    if held_keys['w']:
+        cube.z += 2 * time.dt
+    if held_keys['s']:
+        cube.z -= 2 * time.dt
+    if held_keys['q']:
+        sys.exit()
 
 app.run()
