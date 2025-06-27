b04d4efe4724882633f169c35580563cb18a5434
Ifus0210 <ifus0210@gmail.com>
2025-06-26 18:26:37 -0700

    enable wasd movement in pygame

--- a.py
+++ a.py
@@ -1,12 +1,24 @@
 import pygame
 pygame.init()
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((1000, 1000))
 running = True
+x = 500
+y = 500
+speed = 1
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        y -= speed
+    if keys[pygame.K_s]:
+        y += speed
+    if keys[pygame.K_a]:
+        x -= speed
+    if keys[pygame.K_d]:
+        x += speed
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (255, 0, 0), (100, 100, 50, 50))
+    pygame.draw.rect(screen, (0, 0, 255), (x, y, 50, 50))
     pygame.display.flip()
 pygame.quit()
